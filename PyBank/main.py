7848943f3fa3b3dfe42ef5b70fa7911be79972d6
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2020-09-27 15:54:33 -0600

    rewriting average change formula

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,29 +16,43 @@ with open(budget_data, newline="") as budget:
 
     #Initialize variables & Create and empty list for Date
     date=[]
+    months_total =0
     
     # Finding Net Profit/losses and create an empty list
     profit_loss = []
     total_profit_Loss = 0
 
     # Initialize the variables for Average Changes in Profit/losses 
-  
+    monthly_change = []
+    initial_profit_loss = 0
+    final_profit_loss = 0
+    total_monthly_changes =0
+    average_change = 0
 
     # Loop & Read through each row
     for rows in budget_reader:
 
-    # Calculating Total number of months & adding count of months to months[] empty list
+      # Calculating Total number of months & adding count of months to months[] empty list
       date.append(rows[0])
-      months_total = len(date)
+      months_total += 1
       
-    # Calculating Total amount of "Profit/Losses" & Adding to profit_losst[] empty list & Formatting $ sign in to total
+      # Calculating Total amount of "Profit/Losses" & Adding to profit_losst[] empty list & Formatting $ sign in to total
       profit_loss.append(int(rows[1]))
       total_profit_Loss = int(sum(profit_loss))
-      total_profit_Loss = "${:.0f}".format(total_profit_Loss)
+      total_profit_Loss = "${:.0f}".format(total_profit_Loss)   
 
-    # Calculating Average of changes in Profit/Losses over the entire period
-      average_change = int(sum(profit_loss)) / len(profit_loss)
-
+      # Calculating Average of changes in Profit/Losses from month to month
+      final_profit_loss = int(rows[1])
+      monthly_change_profit_loss = final_profit_loss - initial_profit_loss
+      
+      # Add monthly_change for profit/loss inside monthly_change[] empty list  
+      monthly_change.append(monthly_change_profit_loss)
+      
+      # Calcuating Average Change for entire period
+      total_monthly_changes = total_monthly_changes + monthly_change_profit_loss 
+      initial_profit_loss = final_profit_loss   
+      average_change = (total_monthly_changes / months_total)
+    
     # print out the Results for Financial Analysis
     print("Financial Analysis")
     print("-----------------------------------------------------------------------")
