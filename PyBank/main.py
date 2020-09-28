45d42ba2ec085f77795732eacef9d112318fa7cb
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2020-09-28 11:05:43 -0600

    wrote the code for writing the text file for Financial Analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -34,8 +34,8 @@ with open(budget_data, newline="") as csvbudget:
       # Calculating Total amount of "Profit/Losses" & Adding to profit_losst[] empty list
       profit_loss.append(int(rows[1]))
       total_profit_Loss = int(sum(profit_loss))
-      total_profit_Loss = "${:.0f}".format(total_profit_Loss) 
-# Formatting $ sign in to total and 0 decimal points
+      total_profit_Loss = "${:.0f}".format(total_profit_Loss) # Formatting $ sign in to total and 0 decimal points
+
     # Iterate through the profit_loss list in order to get the monthly change in profits
     for pf in range(len(profit_loss)-1):
 
@@ -58,13 +58,22 @@ greatest_decrease_date = months[monthly_profit_change.index(greatest_decrease_lo
 
 # print out the Results for Financial Analysis
 print("--------------------------------------")
-print(" Financial Analysis:")
+print("     Financial Analysis:")
 print("--------------------------------------")
 print(f'Total Months: {months_total}')
 print(f'Total Profit_Loss: {total_profit_Loss}')
 print(f'Average Change: {average_monthly_change}')
 print(f'Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase_profit})')
-print(f'Greatest Increase in Profits: {greatest_decrease_date} (${greatest_decrease_loss})')
+print(f'Greatest Decrease in Losses: {greatest_decrease_date} (${greatest_decrease_loss})')
 
-# Output files ("Main Folder", "Subfolder","New text file name")
-output_file = path("Python-Challenge","PyBank","Financial_Analysis" )
+# Output text file (file name, 'w':write to file)
+with open("Financial_Analysis_Summary.txt", 'w') as text:
+    text.write("-----------------------------------------------------------\n") # \n next line
+    text.write("  Financial Analysis:"+ "\n")
+    text.write("-----------------------------------------------------------\n\n")
+    text.write(f'       Total Months: {months_total}'+ "\n")
+    text.write(f'       Total Profit_Loss: {total_profit_Loss}'+ "\n")
+    text.write(f'       Average Change: {average_monthly_change}' +"\n")
+    text.write(f'       Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase_profit})'+ "\n")
+    text.write(f'       Greatest Decrease in Losses: {greatest_decrease_date} (${greatest_decrease_loss})'+ "\n\n")
+    text.write("-----------------------------------------------------------\n\n")
