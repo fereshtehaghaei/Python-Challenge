fea2ef2eb1e8be4be718ae1b6635048c5c61dfb4
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2020-10-01 12:49:07 -0600

    added for loop formula for winner

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,18 +13,17 @@ with open(election_data, newline="") as csv_election:
     # Initialize variable and create and empty list[] 
     votes =[]
     total_votes = 0
-    candidate_votes =[]
-    vote_percentage =[]
-    winner_list =[]
+    max_vote = 0
+    winner_candidate=[]
+
     # Creating a dictionary for cadiates
     candidates_dictionary = {}  #candidate name = key, you have to make a mark
     
-
     # Loop & read through each row
     for candidates in election_reader:
         
     # calculating The total number of votes cast
-        #votes.append(candidates[0])
+        votes.append(candidates[0])
         total_votes +=1
         candidate = (candidates[2])  #assigning candidate column 
 
@@ -35,38 +34,30 @@ with open(election_data, newline="") as csv_election:
           candidates_dictionary[candidate]= 1 # candidate in dictionary
         # test print dictionary
     # print(candidates_dictionary)
-  
-# Calculating The percentage of votes each candidate won
-# taking dictionary keys and values and set it up candiate votes list
-#for key, value in candidates_dictionary.items():
-    #candidates.append(key)
-    #candidate_votes.append(value)
-    #vote_percentage = (round(value/total_votes*100,3))
-#for vote in candidate_votes:
-    
-    
-
-#candidates_votes= list(zip(votes, candidate_votes, vote_percentage))
-
-# The winner of the election based on popular vote.   
 
 # print out Eelction Results
+print("**************************************")
 print("     Election Results ")
-print("--------------------------------------")
+print("======================================")
 print(f'Total Votes: {total_votes}')
-print("--------------------------------------")
+print("======================================")
+
 # Created a For loop to iterate through dictionary and print each key
 for candidate, candidates_votes in candidates_dictionary.items():
-    vote_percentage = (round(candidates_votes/total_votes*100,3))
+
+    # Calculating The percentage of votes each candidate won
+    vote_percentage = (round(candidates_votes/total_votes*100,2))
+
+    # finding the winner of the election based on popular vote.
+    if candidates_votes > max_vote:
+       max_vote = candidates_votes
+       candidates = candidate
+       winner_candidate.append(candidate)
     print(candidate + ':  ' + str(vote_percentage) + '% ' + ' ('+str(candidates_votes)+')')
 
-for candidate in candidate_votes:
-    winner_list = max(candidates_votes) == candidates[1]
-      #winner_list.append(candidate[2])
-    print(winner_list)
-      #winner = winner_list[0]
-
-
+print("======================================")
+print(f'Winner:   {candidates}')
+print("======================================")
 """
 
 
